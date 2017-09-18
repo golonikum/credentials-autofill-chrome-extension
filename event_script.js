df000a1f160debf15a98e746118baffefe10b4d6
Nikita Golov <NGolov@veeam.com>
2017-09-18 15:40:10 +0300

    Updated styles, added save message

--- event_script.js
+++ event_script.js
@@ -1,11 +1,27 @@
+var CURRENT_URL = '';
+function getCurrentHost() {
+    return CURRENT_URL.replace(/^(.*:)\/\/([A-Za-z0-9\-\.]+)(:[0-9]+)?(.*)$/, '$2');
+}
+
 chrome.runtime.onInstalled.addListener(function () {
     let credentials = localStorage.getItem('credentials') || JSON.stringify([]);
     localStorage.setItem('credentials', credentials);
 });
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    sendResponse({
-        message: message === 'getCredentials' ? localStorage.getItem('credentials') : '',
-        sender: "event_script.js"
-    });
+    if (message === 'getCredentials') {
+        sendResponse({
+            message: localStorage.getItem('credentials'),
+            sender: "event_script.js"
+        });
+    } else if (message === 'getHost') {
+        sendResponse({
+            message: getCurrentHost(),
+            sender: "event_script.js"
+        });
+    }
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    CURRENT_URL = tab.url;
 });
--- popup_script.js
+++ popup_script.js
@@ -1,15 +1,15 @@
 var formHtml = [
     '<form class="form-inline auto-fill-form">',
         '<div class="form-group">',
-            '<input name="host" class="form-control host-field" placeholder="Host...">',
+            '<input name="host" class="form-control form-control-sm host-field" placeholder="Host...">',
         '</div>',
         '<div class="form-group">',
-            '<input name="user" class="form-control" placeholder="User...">',
+            '<input name="user" class="form-control form-control-sm" placeholder="User...">',
         '</div>',
         '<div class="form-group">',
-            '<input name="password" type="password" class="form-control" placeholder="Password...">',
+            '<input name="password" type="password" class="form-control form-control-sm" placeholder="Password...">',
         '</div>',
-        '<button class="btn btn-danger" title="Remove the row"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>',
+        '<button class="btn btn-danger btn-sm" title="Remove the row"><span class="oi oi-trash" aria-hidden="true"></span> Remove</button>',
     '</form>',
 ].join('');
 
@@ -18,10 +18,33 @@ function onAddClicked() {
     $('#form-container').append(formHtml);
 }
 function onRemoveClicked() {
-    $(this).parents('.auto-fill-form').remove();
+    var form = $(this).parents('.auto-fill-form');
+    form.remove();
 }
 function onSaveClicked() {
+    // chrome.runtime.sendMessage('getHost', function (responseObject) {
+    //     var host = responseObject.message;
+    //     console.log(host);
+    // });
+
     localStorage.setItem('credentials', JSON.stringify(collectCredentials()));
+    $.toast({
+        text: "Changes were successfully saved!", // Text that is to be shown in the toast
+        heading: 'Success', // Optional heading to be shown on the toast
+        icon: 'success', // Type of toast icon
+        showHideTransition: 'fade', // fade, slide or plain
+        allowToastClose: true, // Boolean value true or false
+        hideAfter: 2000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
+        stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
+        position: 'top-center', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
+        textAlign: 'left',  // Text alignment i.e. left, right or center
+        loader: true,  // Whether to show loader or not. True by default
+        loaderBg: '#9EC600',  // Background color of the toast loader
+        beforeShow: function () { }, // will be triggered before the toast is shown
+        afterShown: function () { }, // will be triggered after the toat has been shown
+        beforeHide: function () { }, // will be triggered before the toast gets hidden
+        afterHidden: function () { }  // will be triggered after the toast has been hidden
+    });
 }
 function collectCredentials() {
     let credentials = [];
